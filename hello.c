5df94d95489658fe38af8fe86294014918177522
JoseRLC <111907487+JoseRLC@users.noreply.github.com>
2022-08-27 18:44:40 +0000

    update

--- hello.c
+++ hello.c
@@ -1,11 +1,18 @@
+/**
+ * Jose Lopez-Castaneda
+ * 2022Jun23
+ *
+ * A simple hello world program in C
+ *
+ */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
 
-  printf("Hello I'm Jose Lopez!");
+  printf("Hello I'm Jose Lopez!\n");
 
   return 0;
 }
-   
+
 
